92b57c41862094c9c77030c302c202737b2e671a
Victor <30707373+VicBrnd@users.noreply.github.com>
2024-12-06 21:09:29 +0100

    layout: add vercel analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,24 @@
-// src/app/providers.tsx
-"use client";
 import { Toolbar } from "@/components/toolbar/toolbar";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
 
 interface ProvidersProps {
   children: React.ReactNode;
+  enableAnalytics?: boolean;
 }
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <TooltipProvider>
-      {children}
-      <Toolbar />
-      <Toaster />
-    </TooltipProvider>
+    <>
+      <TooltipProvider>
+        {children}
+        <Toolbar />
+        <Toaster />
+      </TooltipProvider>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
